c9947677c79df1b7114332e6a0cce8ebe19a54ad
dkstpwns10 <dkstpwns10@naver.com>
2026-03-07 23:27:11 +0900

    refactor(outbox) : eventId 타입 변경

--- src/main/kotlin/com/pgcore/core/infra/outbox/domain/OutboxEvent.kt
+++ src/main/kotlin/com/pgcore/core/infra/outbox/domain/OutboxEvent.kt
@@ -4,12 +4,13 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Index
 import jakarta.persistence.Table
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
 import java.time.LocalDateTime
+import java.util.UUID
 
 @Entity
 @Table(
@@ -21,8 +22,8 @@ import java.time.LocalDateTime
 )
 class OutboxEvent protected constructor(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val eventId: Long = 0,
+    @Column(length = 36, nullable = false, updatable = false)
+    val eventId: UUID = UUID.randomUUID(),
 
     @Column(nullable = false, updatable = false)
     val merchantId: Long,
