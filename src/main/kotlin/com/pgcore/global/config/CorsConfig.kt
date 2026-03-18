56cfcc9050c0cdf3c42c3ff571f4ab64b1fb87a6
dkstpwns10 <dkstpwns10@naver.com>
2026-03-18 19:09:59 +0900

    reactor(cors): cors설정 수정

--- src/main/kotlin/com/pgcore/global/config/CorsConfig.kt
+++ src/main/kotlin/com/pgcore/global/config/CorsConfig.kt
@@ -1,25 +1,18 @@
 package com.pgcore.global.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class CorsConfig {
+class CorsConfig : WebMvcConfigurer {
 
-    @Bean
-    fun corsFilter(): CorsFilter {
-        val config = CorsConfiguration().apply {
-            allowedOriginPatterns = listOf("*")
-            allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            allowedHeaders = listOf("*")
-            exposedHeaders = listOf("*")
-            allowCredentials = true
-        }
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", config)
-        return CorsFilter(source)
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders("*")
+            .allowCredentials(true)
     }
 }
